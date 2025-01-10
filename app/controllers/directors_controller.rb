124c7b35f6a2bb884b565bc2e7fcc1257aa61de2
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-01-10 05:13:55 +0000

    director details page done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,11 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list"})
   end
+
+  def show
+    the_id = params.fetch("the_id")
+    @the_director = Director.all.where({ :id => the_id}).at(0)
+
+    render({ :template => "director_templates/details" })
+  end
 end
