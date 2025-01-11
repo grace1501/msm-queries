63d0380084508817b554d847214f026abf73a7e0
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-01-11 02:38:29 +0000

    movies routes done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,8 @@ class MoviesController < ApplicationController
   end
 
   def show
+    the_id = params.fetch("the_id")
+    @the_movie = Movie.all.where({ :id => the_id }).at(0)
     render({ :template => "movie_templates/details"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get("/directors/:the_id", { :controller => "directors", :action => "show" })
 
   get("/movies", { :controller => "movies", :action => "index" })
-  get("/movies", { :controller => "movies", :action => "show" })
+  get("/movies/:the_id", { :controller => "movies", :action => "show" })
 
   
 end
