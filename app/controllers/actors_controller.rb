9f5c6c8c00771c51f3481bd951755c5cf63e0d24
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-01-12 03:09:17 +0000

    characters route done

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,6 +7,8 @@ class ActorsController < ApplicationController
   def show
     the_id = params.fetch("the_id")
     @the_actor = Actor.all.where({ :id => the_id }).at(0)
+    @all_characters = Character.all.where({ :actor_id => the_id })
+
     render({ :template => "actor_templates/details"})
   end
 end
